6551b7eab1482a8ba8056bdb1d20a40b719b4805
Clevesson Mendonça <wanderbloks311@gmail.com>
2023-03-12 22:57:30 -0300

    feat: add MongoClient with mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,27 @@ import express from "express";
 import { config } from "dotenv";
 import { GetImagesController } from "./controllers/get-images/get-images";
 import { MongoGetImagesRepository } from "./repositories/get-images/mongo-get-images";
+import { MongoClient } from "./database/mongo";
 
-config();
+const main = async () => {
+  config();
 
-const app = express();
+  const app = express();
 
-const port = process.env.PORT || 3333;
+  await MongoClient.connect();
 
-app.get("/images", async (req, res) => {
-  const getImagesRepository = new MongoGetImagesRepository();
-  const getImagesController = new GetImagesController(getImagesRepository);
+  app.get("/images", async (req, res) => {
+    const getImagesRepository = new MongoGetImagesRepository();
+    const getImagesController = new GetImagesController(getImagesRepository);
 
-  const { body, statusCode } = await getImagesController.handle();
+    const { body, statusCode } = await getImagesController.handle();
 
-  res.send(body).status(statusCode);
-});
+    res.send(body).status(statusCode);
+  });
 
-app.listen(port, () => console.log("listening on port 3333"));
+  const port = process.env.PORT || 3333;
+
+  app.listen(port, () => console.log("listening on port 3333"));
+};
+
+main();
